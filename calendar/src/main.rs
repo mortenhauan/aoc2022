2314855d6fcaef4b8a347747532d0fdd68257835
Morten Hauan <mortenhauan@Morten-sin-Brbar-datamaskin.local>
2022-12-13 11:15:54 +0100

    Add timing

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -1,9 +1,12 @@
 use colored::Colorize;
+use std::time::Instant;
 
 fn main() {
+    let calendar_timer = Instant::now();
     println!("{}", "Advent of Code 2022".purple().bold());
 
     // Day 01
+    let task_timer = Instant::now();
     println!("\n{}", "Day 1".green().bold());
     println!(
         "The Elf carrying the most calories has {} calories",
@@ -13,8 +16,11 @@ fn main() {
         "The three Elfs carrying the most callories has a total of {} calories",
         day01::task2(include_str!("../input/day01.txt"))
     );
+    let duration = task_timer.elapsed();
+    println!("Time used on Day 01: {:?}", duration);
 
     // Day 02
+    let task_timer = Instant::now();
     println!("\n{}", "Day 2".green().bold());
     println!(
         "If I follow my guessed strategy guide, I will get a score of {}",
@@ -24,8 +30,11 @@ fn main() {
         "If I follow my correct strategy guide, I will get a score of {}",
         day02::task2(include_str!("../input/day02.txt"))
     );
+    let duration = task_timer.elapsed();
+    println!("Time used on Day 02: {:?}", duration);
 
     // Day 03
+    let task_timer = Instant::now();
     println!("\n{}", "Day 3".green().bold());
     println!(
         "The sum of priorities for items are: {}",
@@ -35,4 +44,9 @@ fn main() {
         "The sum of priorities for badges are: {}",
         day03::task2(include_str!("../input/day03.txt"))
     );
+    let duration = task_timer.elapsed();
+    println!("Time used on Day 03: {:?}", duration);
+
+    let duration = calendar_timer.elapsed();
+    println!("\nTime used on Advent of Code 2022: {:?}", duration);
 }
